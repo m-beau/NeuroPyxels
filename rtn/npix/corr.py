4c58c8c24916fdad1ee0c75fb538142b7edd1cd5
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-06-10 11:21:09 +0100

    Update corr.py

--- rtn/npix/corr.py
+++ rtn/npix/corr.py
@@ -4,6 +4,9 @@
 @author: Maxime Beau, Neural Computations Lab, University College London
 """
 
+from ..utils import phyColorsDic, seabornColorsDic, DistinctColors20, DistinctColors15, mark_dict,\
+                    npa, sign, minus_is_1, thresh, smooth, \
+                    _as_array, _unique, _index_of
 from .gl import get_good_units
 from .spk_t import trn, trnb, isi, binarize
 

4280fcbc8dae7dbe2465a74318ed0c6f0a6b5864
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-06-10 10:40:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='routines',
+      version='1.0',
+      description='Maxime Beau personal python routines.',
+      url='https://github.com/m-beau/routines',
+      author='Maxime Beau',
+      author_email='maximebeaujeanroch047@gmail.com',
+      license='MIT',
+      packages=['routines'],
+      package_dir={'routines': 'routines'},
+      keywords='phy,data analysis,electrophysiology,neuroscience')

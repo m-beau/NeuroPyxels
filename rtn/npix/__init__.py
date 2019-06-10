f63f6fa16d4569e62ea4b450cfbf06c2ac55f7ae
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-06-10 17:42:12 +0100

    init files fixed. stats moved.

--- rtn/npix/__init__.py
+++ rtn/npix/__init__.py
@@ -3,3 +3,24 @@
 2019-06-10
 @author: Maxime Beau, Neural Computations Lab, University College London
 """
+
+__doc__="""
+
+npix submodules:
+    npix.io
+    
+    npix.gl
+    
+    npix.spk_t
+    
+    npix.spk_wvf
+    
+    npix.corr
+    
+    npix.plot
+    
+    npix.ml
+    
+    npix.behav
+
+"""

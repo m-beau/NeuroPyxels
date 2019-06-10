b2676a2951b69db07d982a54f811996be7094a72
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-06-10 11:08:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 
 from setuptools import setup
 
-setup(name='routines',
+setup(name='rtn',
       version='1.0',
       description='Maxime Beau personal python routines.',
       url='https://github.com/m-beau/routines',
       author='Maxime Beau',
       author_email='maximebeaujeanroch047@gmail.com',
       license='MIT',
-      packages=['routines'],
-      package_dir={'routines': 'routines'},
+      packages=['rtn', 'rtn.npix'],
+      #package_dir={'rtn': 'rtn'},
       keywords='phy,data analysis,electrophysiology,neuroscience')

a3c4e44d6830905956029533b2617eadb8e0086f
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-06-14 18:18:03 +0100

    Fix gen sfc w/ graph 2

--- rtn/npix/corr.py
+++ rtn/npix/corr.py
@@ -991,6 +991,8 @@ def gen_sfc(dp, cbin=0.2, cwin=100, threshold=2, n_consec_bins=3, rec_section='a
             for u1 in SFCDF.index:
                 for u2 in SFCDF.index:
                     pks=SFCDF.loc[u1, u2]
+                    print(pks)
+                    print(type(pks))
                     if u1<u2 and type(pks) is list:
                         for p in pks:
                             graph.add_edge(u1, u2, u_src=u1, u_trg=u2, 

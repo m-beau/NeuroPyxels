6d600986fd9fb1d25cc061831050142a59744a53
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-06-25 11:16:52 +0100

    Fixed sym CCG significance detection 2

--- rtn/npix/corr.py
+++ rtn/npix/corr.py
@@ -891,7 +891,7 @@ def gen_sfc(dp, cbin=0.2, cwin=100, threshold=2, n_consec_bins=3, rec_section='a
                 for u1 in SFCDF.index:
                     for u2 in SFCDF.index:
                         pks=SFCDF.loc[u1, str(u2)]
-                        if u1==181 or u2==181:
+                        if (u1==181 and u2==165) or (u2==181 and u1==165):
                             print(pks)
                         if u1<u2 and type(pks) is str:
                             pks=ast.literal_eval(pks)

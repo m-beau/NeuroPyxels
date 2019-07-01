9c6c72342dbd39a8bd730b3313468c687afeefaa
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-07-01 12:21:00 +0100

    check

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(name='rtn',
       #package_dir={'rtn': 'rtn'},
       install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn',
       'six', 'elephant', 'vispy', 'statsmodels', 'progressbar',
-      'scikit-learn', 'umap-learn', 'networkx'],
+      'scikit-learn', 'umap-learn', 'networkx', 'psutil'],
       dependency_links=['https://github.com/llerussell/paq2py.git'],
       keywords='phy,data analysis,electrophysiology,neuroscience')

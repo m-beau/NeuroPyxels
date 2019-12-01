ece316a2931a414e8f4d657323c74632e8ee419a
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-12-01 20:37:48 +0000

    testest1

--- rtn/npix/plot.py
+++ rtn/npix/plot.py
@@ -24,6 +24,9 @@ from rtn.npix.corr import acg, ccg, gen_sfc, extract_hist_modulation_features, m
 from mpl_toolkits.mplot3d import axes3d
 from mpl_toolkits.mplot3d import Axes3D
 
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+
 import networkx as nx
 
 #%% regular histogram
@@ -152,7 +155,7 @@ def plot_wvf(dp, u, Nchannels=8, chStart=None, n_waveforms=100, t_waveforms=2.8,
     return fig
 
 def plot_raw(dp, times, channels=np.arange(384), subtype='ap', offset=450, saveDir='~/Downloads', saveData=0, saveFig=0,
-             _format='pdf', color='multi', whiten=True):
+             _format='pdf', color='multi', whiten=True, pyqtgraph=0):
     '''
     ## PARAMETERS
     - bp: binary path (files must ends in .bin, typically ap.bin)
@@ -195,7 +198,7 @@ def plot_raw(dp, times, channels=np.arange(384), subtype='ap', offset=450, saveD
         ax.plot([0, t[0,-1]], [y, y], color=(0.5, 0.5, 0.5), linestyle='--', linewidth=1)
     if color=='multi':
         color=[DistinctColors20[ci%(len(DistinctColors20)-1)] for ci in range(t.shape[0])]
-    ax.plot(t.T, rc.T, linewidth=1, color='k')
+    ax.plot(t.T, rc.T, linewidth=1, color=color)
     ax.set_xlabel('Time (ms)')
     ax.set_ylabel('Extracellular potential (uV)')
     ax.set_yticks(y_ticks)
@@ -207,7 +210,26 @@ def plot_raw(dp, times, channels=np.arange(384), subtype='ap', offset=450, saveD
         saveDir=op.expanduser(saveDir)
         rcn = '{}_t{}-{}_ch{}-{}.npy'.format(op.basename(dp), times[0], times[1], channels[0], channels[-1]) # raw chunk name
         fig.savefig(opj(saveDir, '{}.{}'.format(rcn, _format)), format=_format)
+
+    if pyqtgraph:
+        app = QtGui.QApplication([])
+        
+        win = pg.GraphicsWindow(title="Raw data - {}-{}ms, channels {}-{}".format(times[0], times[1], channels[0], channels[-1]))
+        win.resize(1000,600)
+        p = win.addPlot(title="Multiple curves")
+        # Enable antialiasing for prettier plots
+        pg.setConfigOptions(antialias=True)
     
+        for i in np.arange(rc.shape[0]):
+            y=i*offset
+            p.plot([0, t[0,-1]], [y, y], color=(0.5, 0.5, 0.5), linestyle='--', linewidth=1)
+        if color=='multi':
+            color=[DistinctColors20[ci%(len(DistinctColors20)-1)] for ci in range(t.shape[0])]
+        p.plot(t.T, rc.T, linewidth=1, pen=color)
+        p.setLabel('left', "Extracellular potential", units='uV')
+        p.setLabel('bottom', "Time", units='ms')
+        QtGui.QApplication.instance().exec_()
+        
     return fig
 
 def plot_raw_units(dp, times, channels=np.arange(384), units=[], offset=450, saveDir='~/Downloads', saveData=0, saveFig=0, whiten=1,
@@ -913,96 +935,94 @@ def make_mpl_animation(ax, Nangles, delay, width=10, height=10, saveDir='~/Downl
 
 #%% How to plot 2D things with pyqtplot
     
-# from pyqtgraph.Qt import QtGui, QtCore
-# import numpy as np
-# import pyqtgraph as pg
-
-# #QtGui.QApplication.setGraphicsSystem('raster')
-# app = QtGui.QApplication([])
-# #mw = QtGui.QMainWindow()
-# #mw.resize(800,800)
-
-# win = pg.GraphicsWindow(title="Basic plotting examples")
-# win.resize(1000,600)
-# win.setWindowTitle('pyqtgraph example: Plotting')
-
-# # Enable antialiasing for prettier plots
-# pg.setConfigOptions(antialias=True)
-
-# p1 = win.addPlot(title="Basic array plotting", y=np.random.normal(size=100))
-
-# p2 = win.addPlot(title="Multiple curves")
-# p2.plot(np.random.normal(size=100), pen=(255,0,0), name="Red curve")
-# p2.plot(np.random.normal(size=110)+5, pen=(0,255,0), name="Green curve")
-# p2.plot(np.random.normal(size=120)+10, pen=(0,0,255), name="Blue curve")
-
-# p3 = win.addPlot(title="Drawing with points")
-# p3.plot(np.random.normal(size=100), pen=(200,200,200), symbolBrush=(255,0,0), symbolPen='w')
 
 
-# win.nextRow()
+#QtGui.QApplication.setGraphicsSystem('raster')
+app = QtGui.QApplication([])
+#mw = QtGui.QMainWindow()
+#mw.resize(800,800)
 
-# p4 = win.addPlot(title="Parametric, grid enabled")
-# x = np.cos(np.linspace(0, 2*np.pi, 1000))
-# y = np.sin(np.linspace(0, 4*np.pi, 1000))
-# p4.plot(x, y)
-# p4.showGrid(x=True, y=True)
+win = pg.GraphicsWindow(title="Basic plotting examples")
+win.resize(1000,600)
+win.setWindowTitle('pyqtgraph example: Plotting')
 
-# p5 = win.addPlot(title="Scatter plot, axis labels, log scale")
-# x = np.random.normal(size=1000) * 1e-5
-# y = x*1000 + 0.005 * np.random.normal(size=1000)
-# y -= y.min()-1.0
-# mask = x > 1e-15
-# x = x[mask]
-# y = y[mask]
-# p5.plot(x, y, pen=None, symbol='t', symbolPen=None, symbolSize=10, symbolBrush=(100, 100, 255, 50))
-# p5.setLabel('left', "Y Axis", units='A')
-# p5.setLabel('bottom', "Y Axis", units='s')
-# p5.setLogMode(x=True, y=False)
+# Enable antialiasing for prettier plots
+pg.setConfigOptions(antialias=True)
 
-# p6 = win.addPlot(title="Updating plot")
-# curve = p6.plot(pen='y')
-# data = np.random.normal(size=(10,1000))
-# ptr = 0
-# def update():
-#     global curve, data, ptr, p6
-#     curve.setData(data[ptr%10])
-#     if ptr == 0:
-#         p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
-#     ptr += 1
-# timer = QtCore.QTimer()
-# timer.timeout.connect(update)
-# timer.start(50)
+p1 = win.addPlot(title="Basic array plotting", y=np.random.normal(size=100))
+
+p2 = win.addPlot(title="Multiple curves")
+p2.plot(np.random.normal(size=100), pen=(255,0,0), name="Red curve")
+p2.plot(np.random.normal(size=110)+5, pen=(0,255,0), name="Green curve")
+p2.plot(np.random.normal(size=120)+10, pen=(0,0,255), name="Blue curve")
+
+p3 = win.addPlot(title="Drawing with points")
+p3.plot(np.random.normal(size=100), pen=(200,200,200), symbolBrush=(255,0,0), symbolPen='w')
 
 
-# win.nextRow()
+win.nextRow()
 
-# p7 = win.addPlot(title="Filled plot, axis disabled")
-# y = np.sin(np.linspace(0, 10, 1000)) + np.random.normal(size=1000, scale=0.1)
-# p7.plot(y, fillLevel=-0.3, brush=(50,50,200,100))
-# p7.showAxis('bottom', False)
+p4 = win.addPlot(title="Parametric, grid enabled")
+x = np.cos(np.linspace(0, 2*np.pi, 1000))
+y = np.sin(np.linspace(0, 4*np.pi, 1000))
+p4.plot(x, y)
+p4.showGrid(x=True, y=True)
+
+p5 = win.addPlot(title="Scatter plot, axis labels, log scale")
+x = np.random.normal(size=1000) * 1e-5
+y = x*1000 + 0.005 * np.random.normal(size=1000)
+y -= y.min()-1.0
+mask = x > 1e-15
+x = x[mask]
+y = y[mask]
+p5.plot(x, y, pen=None, symbol='t', symbolPen=None, symbolSize=10, symbolBrush=(100, 100, 255, 50))
+p5.setLabel('left', "Y Axis", units='A')
+p5.setLabel('bottom', "Y Axis", units='s')
+p5.setLogMode(x=True, y=False)
+
+p6 = win.addPlot(title="Updating plot")
+curve = p6.plot(pen='y')
+data = np.random.normal(size=(10,1000))
+ptr = 0
+def update():
+    global curve, data, ptr, p6
+    curve.setData(data[ptr%10])
+    if ptr == 0:
+        p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
+    ptr += 1
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(50)
 
 
-# x2 = np.linspace(-100, 100, 1000)
-# data2 = np.sin(x2) / x2
-# p8 = win.addPlot(title="Region Selection")
-# p8.plot(data2, pen=(255,255,255,200))
-# lr = pg.LinearRegionItem([400,700])
-# lr.setZValue(-10)
-# p8.addItem(lr)
+win.nextRow()
 
-# p9 = win.addPlot(title="Zoom on selected region")
-# p9.plot(data2)
-# def updatePlot():
-#     p9.setXRange(*lr.getRegion(), padding=0)
-# def updateRegion():
-#     lr.setRegion(p9.getViewBox().viewRange()[0])
-# lr.sigRegionChanged.connect(updatePlot)
-# p9.sigXRangeChanged.connect(updateRegion)
-# updatePlot()
+p7 = win.addPlot(title="Filled plot, axis disabled")
+y = np.sin(np.linspace(0, 10, 1000)) + np.random.normal(size=1000, scale=0.1)
+p7.plot(y, fillLevel=-0.3, brush=(50,50,200,100))
+p7.showAxis('bottom', False)
 
-# ## Start Qt event loop unless running in interactive mode or using pyside.
-# if __name__ == '__main__':
-#     import sys
-#     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-#         QtGui.QApplication.instance().exec_()
+
+x2 = np.linspace(-100, 100, 1000)
+data2 = np.sin(x2) / x2
+p8 = win.addPlot(title="Region Selection")
+p8.plot(data2, pen=(255,255,255,200))
+lr = pg.LinearRegionItem([400,700])
+lr.setZValue(-10)
+p8.addItem(lr)
+
+p9 = win.addPlot(title="Zoom on selected region")
+p9.plot(data2)
+def updatePlot():
+    p9.setXRange(*lr.getRegion(), padding=0)
+def updateRegion():
+    lr.setRegion(p9.getViewBox().viewRange()[0])
+lr.sigRegionChanged.connect(updatePlot)
+p9.sigXRangeChanged.connect(updateRegion)
+updatePlot()
+
+## Start Qt event loop unless running in interactive mode or using pyside.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()

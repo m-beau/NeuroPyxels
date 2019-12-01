d1941480ed5dd254158d07699a327c952d0758ad
Maxime Beau <maximebeaujeanroch047@gmail.com>
2019-12-01 20:21:29 +0000

    qwef

--- rtn/npix/gl.py
+++ rtn/npix/gl.py
@@ -14,7 +14,8 @@ import numpy as np
 import pandas as pd
 
 from rtn.utils import npa
-from rtn.npix.io import read_spikeglx_meta
+#from rtn.npix.io import read_spikeglx_meta 11
+import pandas
 
 def assert_multidatasets(dp):
     'Returns unpacked merged_clusters_spikes.npz if it exists in dp, None otherwise.'
--- rtn/utils.py
+++ rtn/utils.py
@@ -1150,27 +1150,4 @@ def _test_graph_cross(window = 11):
     
 def _is_array_like(arr):
     return isinstance(arr, (list, np.ndarray))
-    
-if __name__ == "__main__":
-    from math import pi
-    import pylab
-    
-    i = 10000
-    x = np.linspace(0,3.7*pi,i)
-    y = (0.3*np.sin(x) + np.sin(1.3 * x) + 0.9 * np.sin(4.2 * x) + 0.06 * 
-    np.random.randn(i))
-    y *= -1
-    
-    _max, _min = peakdetect(y, x, 750, 0.30)
-    xm = [p[0] for p in _max]
-    ym = [p[1] for p in _max]
-    xn = [p[0] for p in _min]
-    yn = [p[1] for p in _min]
-    
-    plot = pylab.plot(x, y)
-    pylab.hold(True)
-    pylab.plot(xm, ym, "r+")
-    pylab.plot(xn, yn, "g+")
-    
-    
-    pylab.show()
+

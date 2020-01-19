e7a0696b886e58028ed94491e21c04adf8e0950f
Maxime Beau <maximebeaujeanroch047@gmail.com>
2020-01-19 20:35:42 +0000

    Fix keep_edges function

--- rtn/npix/circuitProphyler.py
+++ rtn/npix/circuitProphyler.py
@@ -513,9 +513,10 @@ class Prophyler:
         
         # Select edges to keep if necessary
         if edges_list is None:
+            print('AHAHAHA', prophylerGraph, src_graph)
             dfe=self.get_edges(frmt='dataframe', prophylerGraph=prophylerGraph, src_graph=src_graph)
             if not any(dfe):
-                print('WARNING prophyler.keep_edges function called but resulted in all edges being kept!')
+                print('WARNING prophyler.keep_edges function called but the provided graph does not seem to have any edges to work on!')
                 return g
             if 'main' in edges_type:
                 if not np.any(dfe):

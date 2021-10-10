de7a09c616bbee1653478e7ed9b721c609fd5f3d
m-beau <maximebeaujeanroch047@gmail.com>
2021-10-10 17:07:10 +0100

    parallel bug fixed

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 requirements=['ipython', 'numpy', 'scipy', 'pandas', 'matplotlib',
-      'six', 'statsmodels', 'cmcrameri', 'tqdm', 'joblib',
+      'six', 'statsmodels', 'cmcrameri', 'tqdm', 'joblib', 'ipywidgets',
       'scikit-learn', 'umap-learn', 'networkx', 'psutil', 'imutils', 'python_utils',
       'h5py', 'numba', 'urllib3', 'certifi', 'idna', 'opencv-python']
 

Fixed h5 units addition/dsmatching

npyx/h5.py
@@ -5,6 +5,7 @@ import sys
 
 import numpy as np
 
+from npyx.utils import assert_int, assert_float
 from npyx.inout import get_npix_sync, chan_map, extract_rawChunk
 from npyx.spk_t import ids, trn
 from npyx.spk_wvf import wvf_dsmatch
@@ -28,7 +29,16 @@ def visititems(group, func):
 
 def visitor_func(name, node):
     if isinstance(node, h5py.Dataset):
-        string = f"{name}: {type(node[()])}"
+        n=node[()]
+        if isinstance(n, bytes):
+            s=n.decode()
+        elif isinstance(n, np.ndarray):
+            s=f"ndarray {n.shape}"
+        elif assert_int(n) or assert_float(n):
+            s=n
+        else:
+            s=type(n)
+        string = f"{name}: {s}"
     else:
         string = name
     print(string)
@@ -82,8 +92,8 @@ def label_unit_h5(h5_path, dataset, unit, label, field='optotagged_label'):
 
 
 def add_unit_h5(h5_path, dp, unit,
-                unit_abolute_id=None, sync_chan_id=None, label=None,
-                again=False, plot_debug=False, verbose=0):
+                unit_abolute_id=None, sync_chan_id=None,
+                again=False, again_wvf=False, plot_debug=False, verbose=False):
     """
     Assumes that dataset id is last folder of directory (yy-mm-dd_iiXXX_probeX).
     """
@@ -98,13 +108,18 @@ def add_unit_h5(h5_path, dp, unit,
     h5_file = h5py.File(h5_path, "a")
 
     # check whether neuron already exists in dataset
-    dataset = Path(dp).name
+    dp=Path(dp)
+    dataset = dp.name
     check_dataset_format(dataset)
     neuron_path = f"datasets/{dataset}/{unit}"
     if neuron_path in h5_file:
         neuron_absolute_path=h5_file[f'{neuron_path}/neuron_absolute_id'][()].decode()
-        print(f"Neuron already in h5 file: {neuron_path} ({neuron_absolute_path})")
-        return neuron_path
+        if again:
+            del h5_file[neuron_path]
+            del h5_file[neuron_absolute_path]
+        else:
+            print(f"Neuron already in h5 file: {neuron_path} ({neuron_absolute_path})")
+            return neuron_path
 
     # figure out where we're at
     if unit_abolute_id is None:
@@ -142,7 +157,7 @@ def add_unit_h5(h5_path, dp, unit,
 
     # waveforms
     dsm_tuple = wvf_dsmatch(dp, unit, t_waveforms=waveform_samples,
-                            again=again, plot_debug=plot_debug, verbose=verbose)
+                            again=again_wvf, plot_debug=plot_debug, verbose=verbose)
     dsm_waveform, peak_chan = dsm_tuple[1], dsm_tuple[3]
     chan_bottom = max(0, peak_chan-11)
     chan_top = min(383, peak_chan+11)

npyx/plot.py
@@ -703,14 +703,14 @@ def plot_wvf(dp, u=None, Nchannels=8, chStart=None, n_waveforms=100, t_waveforms
 
     return fig
 
-def quickplot_n_waves(w, title, peak_channel=None, nchans = 20, fig=None):
+def quickplot_n_waves(w, title='', peak_channel=None, nchans = 20, fig=None):
     "w is a (n_samples, n_channels) array"
     if peak_channel is None:
         pk = np.argmax(np.ptp(w, axis=0))
     else:
         pk = peak_channel
     ylim = [np.min(w[:,pk])-50, np.max(w[:,pk])+50]
-    if fig is None: fig = plt.figure(figsize=(8, 14))
+    if fig is None: fig = plt.figure(figsize=(6, 14))
     chans = np.arange(pk-nchans//2, pk+nchans//2)
     for i in range(nchans):
         ax = plt.subplot(nchans//2, 2, i+1) # will retrieve axes if alrady exists
@@ -724,7 +724,7 @@ def quickplot_n_waves(w, title, peak_channel=None, nchans = 20, fig=None):
         ax.set_ylim(ylim)
         if i%2==1: ax.set_yticklabels([])
         if i<nchans-2: ax.set_xticklabels([])
-    fig.suptitle(title)
+    fig.suptitle(title, y=0.92)
     
     return fig
 

npyx/spk_wvf.py
@@ -291,7 +291,9 @@ def wvf_dsmatch(dp, u, n_waveforms=100, t_waveforms=82, periods='all',
         if verbose: print(f"File {fn} found in routines memory.")
         drift_shift_matched_mean = np.load(Path(dprm,fn_all))
         if plot_debug:
-            fig = quickplot_n_waves(drift_shift_matched_mean, f'dsmatched_waveform {u}')
+            w=wvf(dp, u, n_waveforms=n_waveforms, t_waveforms=t_waveforms)
+            fig = quickplot_n_waves(np.mean(w, 0))
+            fig = quickplot_n_waves(drift_shift_matched_mean, f'blue: 100 random waveforms\norange: dsmatched_waveforms (unit {u})', fig=fig)
         return np.load(Path(dprm,fn)),drift_shift_matched_mean,np.load(Path(dprm,fn_spike_id)), np.load(Path(dprm,fn_peakchan))
 
     ## Extract spike ids so we can extract consecutive waveforms
@@ -456,14 +458,14 @@ def shift_match(waves, alignment_channel,
     """
     
 
-    # pick 5 waveforms of highest amplitude to initialize template
-    # concretely, sort waves array by amplitude and start with first wave
+    # sort waveforms by amplitude
     amplitudes = np.ptp(waves[:,:,alignment_channel], axis=1)
     amplitudes_i = np.argsort(amplitudes, axis=0)
     waves_sort = waves[amplitudes_i[::-1],:,:]
     # use 10 waves of highest amplitude as template
     # most arbitrary decision 0 but seems reasonable and empirically works
-    template = np.mean(waves_sort[:10,:,:], axis=0)
+    n_waveforms_template=10
+    template = np.mean(waves_sort[:n_waveforms_template,:,:], axis=0)
     if recenter_spikes:
         shift = (np.argmax(np.abs(template[:,alignment_channel])) - template.shape[0]//2)%template.shape[0]
         if plot_debug:
@@ -488,7 +490,10 @@ def shift_match(waves, alignment_channel,
         realigned_w = np.concatenate([w[-shift:,:], w[:-shift,:]], axis=0)
         if abs(relative_shift)>max_shift_allowed:
             realigned_w=realigned_w*np.nan
-
+        if plot_debug and i==0:
+            fig=imshow_cbar(template)
+            fig=imshow_cbar(w_closestchannels)
+            fig=imshow_cbar(xcorr_w_template)
         # store realigned_wave in array
         aligned_waves[i,:,:] = realigned_w
 

npyx/utils.py
@@ -81,12 +81,13 @@ _ACCEPTED_ARRAY_DTYPES = (np.float, np.float32, np.float64,
                           np.bool)
 
 def assert_float(x):
-    return type(x) in [float, np.float, np.float16,
-                       np.float32, np.float64]
+    return isinstance(x, (float, np.float,
+                          np.float16, np.float32, np.float64))
 
 def assert_int(x):
-    return type(x) in [int, np.int, np.int8, np.int16, np.uint8,
-                       np.uint16, np.int32, np.int64, np.uint32, np.uint64]
+    return isinstance(x, (int, np.int, np.int8, np.int16,
+                          np.uint8, np.uint16, np.int32,
+                          np.int64, np.uint32, np.uint64))
 
 def assert_iterable(x):
     return hasattr(x, '__iter__')
@@ -473,6 +474,7 @@ def xcorr_1d_loop(w1, w2):
     c = np.zeros(w1.shape)
     for i, (wave1, wave2) in enumerate(zip(normalize(w1,0).T, normalize(w2,0).T)):
         c[:,i] = np.correlate(wave1, wave2, mode='same')
+    c[np.isnan(c)]=0 # replace nans with 0 correlation
     return c
 
 def xcorr_2d(w1, w2):
@@ -489,7 +491,9 @@ def normalize(x, axis=0):
     """
     Vanilla normalization (center, reduce) along specified axis
     """
-    return (x-np.mean(x, axis=axis))/np.std(x, axis=axis)
+    s = np.std(x, axis=axis)
+    m = np.mean(x, axis=axis)
+    return (x-m)/s
 
 def get_bins(cwin, cbin):
     mod2=(cwin/cbin)%2
81f19e1f9df390eab6f240cc5797920afd93d4ae
m-beau <maximebeaujeanroch047@gmail.com>
2022-06-13 15:15:08 +0100

    fixed mmap closing Windows bug

--- npyx/h5.py
+++ npyx/h5.py
@@ -7,7 +7,7 @@ import warnings
 import numpy as np 
 
 from npyx.utils import assert_int, assert_float
-from npyx.inout import get_npix_sync, chan_map, extract_rawChunk, read_metadata
+from npyx.inout import get_binary_file_path, get_npix_sync, chan_map, extract_rawChunk, read_metadata
 from npyx.spk_t import ids, trn, trn_filtered
 from npyx.spk_wvf import wvf_dsmatch
 from npyx.gl import get_units, check_periods
@@ -50,6 +50,7 @@ def add_unit_h5(h5_path, dp, unit, lab_id, periods=[[0,20*60]],
                 dataset=None, snr_window=[0.1, 30.1], center_snw_window_on_spikes=True,
                 raw_snippet_halfrange=2,
                 optostims=None, optostims_threshold=None, n_waveforms_for_matching=5000,
+                include_raw_snippets=True,
                 **kwargs):
     """
     Add a Kilosort sorted unit to an HDF5 file.
@@ -114,173 +115,177 @@ def add_unit_h5(h5_path, dp, unit, lab_id, periods=[[0,20*60]],
     waveform_samples = int(waveform_samples*samp_rate/1000)
 
     # open file in append mode
-    h5_file = h5py.File(h5_path, "a")
+    with h5py.File(h5_path, "a") as h5_file:
 
-    # check whether neuron already exists in dataset
-    if dataset is None:
-        dataset = dp.name
-    check_dataset_format(dataset)
-    neuron_path = f"datasets/{dataset}/{unit}"
-    if neuron_path in h5_file:
-        neuron_absolute_path=h5_file[f'{neuron_path}/neuron_absolute_id'][()].decode()
-        if again:
-            del h5_file[neuron_path]
-            del h5_file[neuron_absolute_path]
-        else:
-            print(f"Neuron found in h5 file: {neuron_path} ({neuron_absolute_path})")
-            neuron_group = h5_file[neuron_absolute_path]
-            #return neuron_path
-        
-    if neuron_path not in h5_file or again:
-        # figure out where we're at
-        if unit_abolute_id is None:
-            if f"{lab_id}_neuron_0" not in h5_file:
-                unit_abolute_id = 0
+        # check whether neuron already exists in dataset
+        if dataset is None:
+            dataset = dp.name
+        check_dataset_format(dataset)
+        neuron_path = f"datasets/{dataset}/{unit}"
+        if neuron_path in h5_file:
+            neuron_absolute_path=h5_file[f'{neuron_path}/neuron_absolute_id'][()].decode()
+            if again:
+                del h5_file[neuron_path]
+                del h5_file[neuron_absolute_path]
             else:
-                root_groups = list(h5_file.keys())
-                neuron_ids = [int(x.split('_')[-1]) for x in root_groups if f"{lab_id}_neuron" in x]
-                unit_abolute_id = np.sort(neuron_ids)[-1] + 1
+                print(f"Neuron found in h5 file: {neuron_path} ({neuron_absolute_path})")
+                neuron_group = h5_file[neuron_absolute_path]
+                #return neuron_path
+            
+        if neuron_path not in h5_file or again:
+            # figure out where we're at
+            if unit_abolute_id is None:
+                if f"{lab_id}_neuron_0" not in h5_file:
+                    unit_abolute_id = 0
+                else:
+                    root_groups = list(h5_file.keys())
+                    neuron_ids = [int(x.split('_')[-1]) for x in root_groups if f"{lab_id}_neuron" in x]
+                    unit_abolute_id = np.sort(neuron_ids)[-1] + 1
 
-        # create group for new neuron
-        neuron_absolute_path=f'{lab_id}_neuron_{unit_abolute_id}'
-        neuron_group = h5_file.create_group(neuron_absolute_path)
-        h5_file[neuron_path] = neuron_group
-        print(f"Adding data at {neuron_path} ({neuron_absolute_path})...")
+            # create group for new neuron
+            neuron_absolute_path=f'{lab_id}_neuron_{unit_abolute_id}'
+            neuron_group = h5_file.create_group(neuron_absolute_path)
+            h5_file[neuron_path] = neuron_group
+            print(f"Adding data at {neuron_path} ({neuron_absolute_path})...")
 
-    # metadata
-    add_dataset_to_group(neuron_group, 'lab_id', lab_id, again)
-    add_dataset_to_group(neuron_group, 'dataset_id', dataset, again)
-    add_dataset_to_group(neuron_group, 'neuron_id', unit, again)
-    add_dataset_to_group(neuron_group, 'neuron_absolute_id', neuron_group.name, again)
-    add_dataset_to_group(neuron_group, 'sampling_rate', samp_rate, again)
-    
-    # add any additional keys passed to this function
-    for key, value in kwargs.items():
-        add_dataset_to_group(neuron_group, key, value, again)
-
-    # spike_times
-    periods = check_periods(periods)
-    if 'spike_indices' not in neuron_group or again:
-        t = trn(dp, unit, periods=periods, again=again)
-        add_dataset_to_group(neuron_group, 'spike_indices', t, again)
-        if optostims is None:
-            ons, offs = get_npix_sync(dp, verbose=False)
-            if sync_chan_id is None:
-                sync_chan_id = get_stim_chan(ons)
-            ons, offs = ons[sync_chan_id], offs[sync_chan_id]
-            if ons[0] > offs[0]:
-                ons, offs = offs, ons 
-            if len(offs) == len(ons) - 1:
-                offs = np.append(offs, meta['recording_length_seconds'])
-            optostims = np.hstack([ons[:, None], offs[:, None]])
-        if optostims_threshold is not None:
-            opto_m = optostims[:,0] > optostims_threshold
-            optostims = optostims[opto_m,:]
-        add_dataset_to_group(neuron_group, 'optostims', optostims, again)
-        # Only consider spikes 10s before first opto onset
-        sane_spikes = (t < (optostims[0,0]-10)*samp_rate)
-        add_dataset_to_group(neuron_group, 'sane_spikes', sane_spikes, again)
+        # metadata
+        add_dataset_to_group(neuron_group, 'lab_id', lab_id, again)
+        add_dataset_to_group(neuron_group, 'dataset_id', dataset, again)
+        add_dataset_to_group(neuron_group, 'neuron_id', unit, again)
+        add_dataset_to_group(neuron_group, 'neuron_absolute_id', neuron_group.name, again)
+        add_dataset_to_group(neuron_group, 'sampling_rate', samp_rate, again)
         
-    if 'fn_fp_filtered_spikes' not in neuron_group or again:
-        # get good spikes mask for all spikes
-        # because trn_filtered can only work on a contiguous chunk
-        if periods is 'all':
-            periods_m_range = [0, meta['recording_length_seconds']/60]
-        else:
-            periods_m_range = [periods.min()/60, periods.max()/60]
-        fp_fn_good_spikes = trn_filtered(dp, unit, plot_debug=plot_debug, again=again, period_m=periods_m_range)[1]
+        # add any additional keys passed to this function
+        for key, value in kwargs.items():
+            add_dataset_to_group(neuron_group, key, value, again)
+
+        # spike_times
+        periods = check_periods(periods)
+        if 'spike_indices' not in neuron_group or again:
+            t = trn(dp, unit, periods=periods, again=again)
+            add_dataset_to_group(neuron_group, 'spike_indices', t, again)
+            if optostims is None:
+                ons, offs = get_npix_sync(dp, verbose=False)
+                if sync_chan_id is None:
+                    sync_chan_id = get_stim_chan(ons)
+                ons, offs = ons[sync_chan_id], offs[sync_chan_id]
+                if ons[0] > offs[0]:
+                    ons, offs = offs, ons 
+                if len(offs) == len(ons) - 1:
+                    offs = np.append(offs, meta['recording_length_seconds'])
+                optostims = np.hstack([ons[:, None], offs[:, None]])
+            if optostims_threshold is not None:
+                opto_m = optostims[:,0] > optostims_threshold
+                optostims = optostims[opto_m,:]
+            add_dataset_to_group(neuron_group, 'optostims', optostims, again)
+            # Only consider spikes 10s before first opto onset
+            sane_spikes = (t < (optostims[0,0]-10)*samp_rate)
+            add_dataset_to_group(neuron_group, 'sane_spikes', sane_spikes, again)
+            
+        if 'fn_fp_filtered_spikes' not in neuron_group or again:
+            # get good spikes mask for all spikes
+            # because trn_filtered can only work on a contiguous chunk
+            if periods is 'all':
+                periods_m_range = [0, meta['recording_length_seconds']/60]
+            else:
+                periods_m_range = [periods.min()/60, periods.max()/60]
+            fp_fn_good_spikes = trn_filtered(dp, unit, plot_debug=plot_debug, again=again, period_m=periods_m_range)[1]
 
 
-        # if periods is not all, trim down the mask to spikes in periods
-        if periods is not 'all':
-            t = trn(dp, unit, periods=periods) # if again, as recomputed just above anyway, so don't pass the argument
-            t_all = trn(dp, unit) # grab all spikes
-            periods_mask = np.isin(t_all, t)
-            fp_fn_good_spikes = fp_fn_good_spikes[periods_mask]
+            # if periods is not all, trim down the mask to spikes in periods
+            if periods is not 'all':
+                t = trn(dp, unit, periods=periods) # if again, as recomputed just above anyway, so don't pass the argument
+                t_all = trn(dp, unit) # grab all spikes
+                periods_mask = np.isin(t_all, t)
+                fp_fn_good_spikes = fp_fn_good_spikes[periods_mask]
 
-        add_dataset_to_group(neuron_group, 'fn_fp_filtered_spikes', fp_fn_good_spikes, again)
+            add_dataset_to_group(neuron_group, 'fn_fp_filtered_spikes', fp_fn_good_spikes, again)
 
-    # waveforms
-    if 'mean_waveform_preprocessed' not in neuron_group\
-        or ('amplitudes' not in neuron_group)\
+        # waveforms
+        if 'mean_waveform_preprocessed' not in neuron_group\
+            or ('amplitudes' not in neuron_group)\
+            or ('voltage_sample' not in neuron_group)\
+            or again: # must recompute chan_bottom and chan_top - suboptimal, can be rewritten
+            dsm_tuple = wvf_dsmatch(dp, unit, t_waveforms=waveform_samples, periods=periods,
+                                    again=again_wvf, plot_debug=plot_debug, verbose=verbose,
+                                    n_waves_used_for_matching=n_waveforms_for_matching)
+            dsm_waveform, peak_chan = dsm_tuple[1], dsm_tuple[3]
+            add_dataset_to_group(neuron_group, 'primary_channel', peak_chan)
+            chan_bottom = max(0, peak_chan-11)
+            chan_top = min(383, peak_chan+11)
+            dsm_waveform_chunk = dsm_waveform[:, chan_bottom:chan_top]
+            add_dataset_to_group(neuron_group, 'mean_waveform_preprocessed', dsm_waveform_chunk.T, again)
+            add_dataset_to_group(neuron_group, 'consensus_waveform', dsm_waveform_chunk.T*np.nan, again)
+            cm = chan_map(dp)
+            add_dataset_to_group(neuron_group, 'channel_ids', np.arange(chan_bottom, chan_top, dtype=np.dtype('uint16')), again)
+            add_dataset_to_group(neuron_group, 'channelmap', cm[chan_bottom:chan_top, 1:2], again)
+
+        # Extract voltage snippets
+        if ('amplitudes' not in neuron_group)\
         or ('voltage_sample' not in neuron_group)\
-        or again: # must recompute chan_bottom and chan_top - suboptimal, can be rewritten
-        dsm_tuple = wvf_dsmatch(dp, unit, t_waveforms=waveform_samples, periods=periods,
-                                again=again_wvf, plot_debug=plot_debug, verbose=verbose,
-                                n_waves_used_for_matching=n_waveforms_for_matching)
-        dsm_waveform, peak_chan = dsm_tuple[1], dsm_tuple[3]
-        add_dataset_to_group(neuron_group, 'primary_channel', peak_chan)
-        chan_bottom = max(0, peak_chan-11)
-        chan_top = min(383, peak_chan+11)
-        dsm_waveform_chunk = dsm_waveform[:, chan_bottom:chan_top]
-        add_dataset_to_group(neuron_group, 'mean_waveform_preprocessed', dsm_waveform_chunk.T, again)
-        add_dataset_to_group(neuron_group, 'consensus_waveform', dsm_waveform_chunk.T*np.nan, again)
-        cm = chan_map(dp)
-        add_dataset_to_group(neuron_group, 'channel_ids', np.arange(chan_bottom, chan_top, dtype=np.dtype('uint16')), again)
-        add_dataset_to_group(neuron_group, 'channelmap', cm[chan_bottom:chan_top, 1:2], again)
+        or again:
+            if center_snw_window_on_spikes:
+                t = h5_file[neuron_path+'/spike_indices'][...]/samp_rate
+                if snr_window[1]>t[0]: # spike starting after end of original window
+                    snr_window = np.array(snr_window)+t[0]
+                    snr_window[1]=min(snr_window[1], t[-1])
+            chunk = extract_rawChunk(dp, snr_window, channels=np.arange(chan_bottom, chan_top), 
+                                        scale=False, med_sub=False, whiten=False, center_chans_on_0=False,
+                                        hpfilt=False, verbose=False)
 
-    # Extract voltage snippets
-    if ('amplitudes' not in neuron_group)\
-       or ('voltage_sample' not in neuron_group)\
-       or again:
-        if center_snw_window_on_spikes:
-            t = h5_file[neuron_path+'/spike_indices'][...]/samp_rate
-            if snr_window[1]>t[0]: # spike starting after end of original window
-                snr_window = np.array(snr_window)+t[0]
-                snr_window[1]=min(snr_window[1], t[-1])
-        chunk = extract_rawChunk(dp, snr_window, channels=np.arange(chan_bottom, chan_top), 
-                                    scale=False, med_sub=False, whiten=False, center_chans_on_0=False,
-                                    hpfilt=False, verbose=False)
-
-    # quality metrics
-    if 'amplitudes' not in neuron_group or again:
-        add_dataset_to_group(neuron_group, 'amplitudes', np.load(dp/'amplitudes.npy').squeeze()[ids(dp, unit, periods=periods)], again)
-        mad = np.median(np.abs(chunk) - np.median(chunk, axis=1)[:, None], axis=1) 
-        std_estimate = (mad / 0.6745) # Convert to std
-        add_dataset_to_group(neuron_group, 'channel_noise_std', std_estimate, again)
-    
-    # voltage snippets
-    if 'voltage_sample' not in neuron_group or again:
-        # Only store the voltage sample for the primary channel
-        peak_chan = neuron_group['primary_channel']
-        raw_snippet_halfrange = np.clip(raw_snippet_halfrange, 0, 10)
-        c1, c2 = max(0,int(chunk.shape[0]/2-raw_snippet_halfrange)), min(chunk.shape[0]-1, int(chunk.shape[0]/2+raw_snippet_halfrange+1))
-        raw_snippet = chunk[c1:c2,:]
-        add_dataset_to_group(neuron_group, 'voltage_sample', raw_snippet) # still centered on peak channel, but half the size
-        add_dataset_to_group(neuron_group, 'voltage_sample_start_index', int(snr_window[0] * samp_rate))
-        add_dataset_to_group(neuron_group, 'scaling_factor', meta['bit_uV_conv_factor'])
+        # quality metrics
+        if 'amplitudes' not in neuron_group or again:
+            add_dataset_to_group(neuron_group, 'amplitudes', np.load(dp/'amplitudes.npy').squeeze()[ids(dp, unit, periods=periods)], again)
+            mad = np.median(np.abs(chunk) - np.median(chunk, axis=1)[:, None], axis=1) 
+            std_estimate = (mad / 0.6745) # Convert to std
+            add_dataset_to_group(neuron_group, 'channel_noise_std', std_estimate, again)
         
-    if 'whitened_voltage_sample' not in neuron_group or again:
-        if center_snw_window_on_spikes:
-            t = h5_file[neuron_path+'/spike_indices'][...]/samp_rate
-            if snr_window[1]>t[0]: # spike starting after end of original window
-                snr_window = np.array(snr_window)+t[0]
-                snr_window[1]=min(snr_window[1], t[-1])
-        white_chunk = extract_rawChunk(dp, snr_window, channels=np.arange(chan_bottom, chan_top), 
-                                scale=True, med_sub=False, hpfilt=True, filter_forward=False, filter_backward=True,
-                                whiten=True, use_ks_w_matrix=True,
-                                verbose=False)
-        raw_snippet_halfrange = np.clip(raw_snippet_halfrange, 0, 10)
-        c1 = max(0,int(white_chunk.shape[0]/2-raw_snippet_halfrange))
-        c2 = min(white_chunk.shape[0]-1, int(white_chunk.shape[0]/2+raw_snippet_halfrange+1))
-        raw_snippet = white_chunk[c1:c2,:].astype(np.float32)
-        add_dataset_to_group(neuron_group, 'whitened_voltage_sample', raw_snippet)
+        # voltage snippets
+        if 'voltage_sample' not in neuron_group or again and include_raw_snippets:
+            # Only store the voltage sample for the primary channel
+            peak_chan = neuron_group['primary_channel']
+            raw_snippet_halfrange = np.clip(raw_snippet_halfrange, 0, 10)
+            c1, c2 = max(0,int(chunk.shape[0]/2-raw_snippet_halfrange)), min(chunk.shape[0]-1, int(chunk.shape[0]/2+raw_snippet_halfrange+1))
+            raw_snippet = chunk[c1:c2,:]
+            add_dataset_to_group(neuron_group, 'voltage_sample', raw_snippet) # still centered on peak channel, but half the size
+            add_dataset_to_group(neuron_group, 'voltage_sample_start_index', int(snr_window[0] * samp_rate))
+            add_dataset_to_group(neuron_group, 'scaling_factor', meta['bit_uV_conv_factor'])
+            
+        if 'whitened_voltage_sample' not in neuron_group or again and include_raw_snippets:
+            if center_snw_window_on_spikes:
+                t = h5_file[neuron_path+'/spike_indices'][...]/samp_rate
+                if snr_window[1]>t[0]: # spike starting after end of original window
+                    snr_window = np.array(snr_window)+t[0]
+                    snr_window[1]=min(snr_window[1], t[-1])
+            # check that file filtered properly
+            bin_f = get_binary_file_path(dp)
+            if 'medsub' not in bin_f:
+                warnings.warn(f"WARNING file {bin_f} is expected to have been median subtracted but its file name does not contain medsub...")
+            if 'tempfiltNone300' not in bin_f:
+                warnings.warn(f"WANRING file {bin_f} ")
+            # reprocess it
+            white_chunk = extract_rawChunk(dp, snr_window, channels=np.arange(chan_bottom, chan_top), 
+                                    scale=True, med_sub=False, hpfilt=True, filter_forward=False, filter_backward=True,
+                                    whiten=True, use_ks_w_matrix=True,
+                                    verbose=False)
+            raw_snippet_halfrange = np.clip(raw_snippet_halfrange, 0, 10)
+            c1 = max(0,int(white_chunk.shape[0]/2-raw_snippet_halfrange))
+            c2 = min(white_chunk.shape[0]-1, int(white_chunk.shape[0]/2+raw_snippet_halfrange+1))
+            raw_snippet = white_chunk[c1:c2,:].astype(np.float32)
+            add_dataset_to_group(neuron_group, 'whitened_voltage_sample', raw_snippet)
 
-    # layer
-    add_dataset_to_group(neuron_group, 'phyllum_layer', 0, again)
-    add_dataset_to_group(neuron_group, 'human_layer', 0, again)
+        # layer
+        add_dataset_to_group(neuron_group, 'phyllum_layer', 0, again)
+        add_dataset_to_group(neuron_group, 'human_layer', 0, again)
 
-    # ground truth labels
-    add_dataset_to_group(neuron_group, 'expert_label', 0, again)
-    add_dataset_to_group(neuron_group, 'optotagged_label', 0, again)
+        # ground truth labels
+        add_dataset_to_group(neuron_group, 'expert_label', 0, again)
+        add_dataset_to_group(neuron_group, 'optotagged_label', 0, again)
 
-    # predicted labels
-    add_dataset_to_group(neuron_group, 'lisberger_label', 0, again)
-    add_dataset_to_group(neuron_group, 'hausser_label', 0, again)
-    add_dataset_to_group(neuron_group, 'medina_label', 0, again)
-
-    # close file
-    h5_file.close()
+        # predicted labels
+        add_dataset_to_group(neuron_group, 'lisberger_label', 0, again)
+        add_dataset_to_group(neuron_group, 'hausser_label', 0, again)
+        add_dataset_to_group(neuron_group, 'medina_label', 0, again)
 
     return neuron_path
 

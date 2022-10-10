e5c2ec8ee1c764b2364fe2853608d2f8ffd4919f
m-beau <maximebeaujeanroch047@gmail.com>
2022-10-10 20:35:10 +0100

    sync

---
+++ npyx/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from . import inout, utils, gl, spk_t, spk_wvf, corr, plot, behav, merger, stats
+
+from .utils import *
+from .inout import *
+from .gl import *
+from .spk_t import *
+from .spk_wvf import *
+from .corr import *
+from .stats import *
+from .plot import *
+from .behav import *
+from .merger import *
+from .circuitProphyler import *
+from .feat import *
+from .info import *
+from .h5 import *
+from .testing import *
+
+__doc__="""
+
+npyx submodules:
+ .utils
+ .io
+ .gl
+ .spk_t
+ .spk_wvf
+ .corr
+ .stats
+ .plot
+ .behav
+ .merger
+ .circuitProphyler
+ .feat
+ .h5
+"""
+
+__version__ = '2.6.1'
+
+print(f'npyx version {__version__} imported.')

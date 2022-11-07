ff41b70629b5780ca4c65759d5bb6743cc230b87
BRANDON STELL <brandon.stell@parisdescartes.fr>
2022-11-07 17:10:35 +0100

    Allow plotting in a specific figure.

--- npyx/plot.py
+++ npyx/plot.py
@@ -1144,7 +1144,7 @@ def plot_raw_units(dp, times, units=[], channels=np.arange(384), offset=450,
                    whiten=False, nRangeWhiten=None, med_sub=False, nRangeMedSub=None, hpfilt=0, hpfiltf=300,
                    filter_forward=False, filter_backward=False,ignore_ks_chanfilt=0,
                    show_allyticks=0, yticks_jump=50, plot_ylabels=True, events=[], set0atEvent=1,
-                   again=False):
+                   again=False, ax=None):
     f'''
     Plot raw traces with colored overlaid spike times of specified units.
 
@@ -1189,8 +1189,8 @@ def plot_raw_units(dp, times, units=[], channels=np.arange(384), offset=450,
              ax=None, ext_data=None, ext_datachans=np.arange(384),
              as_heatmap=False, vmin=-50,vmax=50,center=0)
         
-
-    ax=fig.get_axes()[0]
+    if ax is None:
+        ax=fig.get_axes()[0]
     assert assert_iterable(units)
     assert len(units)>=1
     fs=read_metadata(dp)['highpass']['sampling_rate']

Fixed thresholding function bugs when handling very small values, close to float64 precision limits.

npyx/spk_t.py
@@ -54,7 +54,11 @@ def ids(dp, unit, sav=True, verbose=False, periods='all',
     '''
 
     dp = Path(dp)
-    assert unit in get_units(dp), f'WARNING unit {unit} not found in dataset {dp}!'
+    err_mess = f'WARNING unit {unit} not found in dataset {dp}!'
+    try:
+        assert unit in get_units(dp), err_mess
+    except AssertionError:
+        assert unit in get_units(dp, again=True), err_mess
 
     # DEPRECATED now caching with cachecache
     # # Search if the variable is already saved in dp/routinesMemory
@@ -69,8 +73,7 @@ def ids(dp, unit, sav=True, verbose=False, periods='all',
     # if verbose: print(f"File {fn} not found in routines memory. Will be computed from source files.")
     if not (assert_int(unit)|assert_float(unit)):
         raise TypeError(f'WARNING unit {unit} type ({type(unit)}) not handled!')
-    assert unit in get_units(dp),\
-        f'WARNING unit {unit} not found in dataset {dp}!'
+
     # if assert_multi(dp):
     #     ds_table = get_ds_table(dp)
     #     if ds_table.shape[0]>1: # If merged dataset
@@ -164,7 +167,11 @@ def trn(dp, unit, sav=True, verbose=False,
         # DEPRECATED now caching with cachecache
         #if verbose: print(f"File {fn} not found in routines memory. Will be computed from source files.")
         if not (assert_int(unit)|assert_float(unit)): raise TypeError(f'WARNING unit {unit} type ({type(unit)}) not handled!')
-        assert unit in get_units(dp), f'WARNING unit {unit} not found in dataset {dp}!'
+        err_mess = f'WARNING unit {unit} not found in dataset {dp}!'
+        try:
+            assert unit in get_units(dp), err_mess
+        except AssertionError:
+            assert unit in get_units(dp, again=True), err_mess
 
         spike_clusters = np.load(Path(dp,"spike_clusters.npy"), mmap_mode='r')
         spike_samples = np.load(Path(dp,'spike_times.npy'), mmap_mode='r')
@@ -190,7 +197,7 @@ def trn(dp, unit, sav=True, verbose=False,
             sec_bool = sec_bool|(train>=section[0])&(train<=section[1])
         train=train[sec_bool]
 
-    return train
+    return train.astype(np.int64)
 
 def duplicates_mask(t, enforced_rp=0, fs=30000):
     '''

npyx/utils.py
@@ -334,21 +334,48 @@ def thresh_fast(arr, th, sgn=1, pos=1):
 
 def thresh_consecutive(arr, th, sgn=1, n_consec=0,
                        exclude_edges=True, ret_values=True):
-    '''
-    Returns indices and values of threshold crosses lasting >= n_consec consecutive samples in arr.
+    """
+    Finds consecutive threshold crossings in a 1D array.
+
+    This function identifies consecutive elements in the input array `arr` that cross
+    a specified threshold `th` in a given direction (above or below) determined by `sgn`.
+    It returns the indices and, optionally, the values of the threshold crossings that
+    last for at least `n_consec` consecutive elements.
+
+    Usage example:
+        >>> arr = np.array([1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5])
+        >>> th = 3
+        >>> sgn = 1
+        >>> n_consec = 2
+        >>> exclude_edges = True
+        >>> ret_values = True
+        >>> crosses = thresh_consecutive(arr, th, sgn, n_consec, exclude_edges, ret_values)
+        >>> print(crosses)
+        [array([[3, 4, 5],
+                [3, 4, 5]]), array([[10, 11, 12],
+                                    [3, 4, 5]])]
+
     Arguments:
-        - arr: numpy array to threshold
-        - th: float, threshold
-        - sgn: 1 or -1, positive (for cases above threshold) or negative (below threshold)
-        - n_consec: optional int, minimum number of consecutive elements beyond threshold | Defult 0 (any cross)
-        - exclude_edges: bool, if true edges of arr are not considered as threshold crosses
-                            in cases where arr starts or ends beyond threshold
-        - ret_values: bool, whether to return crosses values (list of 2d np arrays [indices, array values] of len Ncrosses)
-                            rather than mere crosses indices (2d np array of shape (Ncrosses, 2))
+        - arr (numpy.ndarray): 1D input array to be thresholded.
+        - th (float): Threshold value.
+        - sgn (int, optional): Sign of the threshold crossing.
+            1 for positive crossings (default), -1 for negative crossings.
+        - n_consec (int, optional): Minimum number of consecutive elements required for a
+            valid threshold crossing (default: 0, any crossing).
+        - exclude_edges (bool, optional): If True, excludes threshold crossings that occur
+            at the edges of the array (default: True).
+        - ret_values (bool, optional): If True, returns the values of the threshold crossings
+            in addition to the indices (default: True).
+
     Returns:
-        - crosses values, list of 2d np arrays [indices, array values] if ret_values is True
-                or indices, 2d np array of shape (Ncrosses, 2) if ret_values is False
-    '''
+        - list: If `ret_values` is True, returns a list of 2D numpy arrays, where each array
+            represents a threshold crossing and contains the indices and values of the
+            crossing. The shape of each array is (2, n), where n is the number of elements
+            in the crossing.
+            If `ret_values` is False, returns a list of 2D numpy arrays, where each array
+            represents a threshold crossing and contains the start and end indices of the
+            crossing. The shape of each array is (2,).
+    """
     
     # argument processing
     arr = npa(arr)
@@ -360,10 +387,16 @@ def thresh_consecutive(arr, th, sgn=1, n_consec=0,
 
     # Simply flip the array around threshold if sgn is -1
     # and threshold positive crossings in every case!
-    arr = (arr - th) * sgn + th
+    # BUG DISCOVERED - if very small values (e.g. p values, e-30), array precision error.
+    # NOW, sgn handled at the crossing selection stage.
+    #arr = (arr - th) * sgn + th
 
     # find crossings
-    cross_thp, cross_thn = thresh(arr, th, 1, 1), thresh(arr, th, -1, -1)
+    # cross_thp, cross_thn = thresh(arr, th, 1, 1), thresh(arr, th, -1, -1)
+    if sgn == 1:
+        cross_thp, cross_thn = thresh(arr, th, 1, 1), thresh(arr, th, -1, -1)
+    else:
+        cross_thp, cross_thn = thresh(arr, th, -1, 1), thresh(arr, th, 1, -1)
     n_pos_crosses = len(cross_thp)
     n_neg_crosses = len(cross_thn)
     n_crosses_total = n_pos_crosses + n_neg_crosses
@@ -407,6 +440,7 @@ def thresh_consecutive(arr, th, sgn=1, n_consec=0,
                 ]
         
     return crosses
+
 
 def thresh_consec(arr, th, sgn=1, n_consec=0,
                   exclude_edges=True, only_max=False, ret_values=True):